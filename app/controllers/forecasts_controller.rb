195c558a9f42e3a90d07576cfc191e926b7bac7f
Alfonso Juan Dillera <alfonsod@sourcepad.com>
2017-02-19 00:02:18 +0800

    Added cassandra service and fixed bug with pickadate.

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,4 +1,25 @@
 class ForecastsController < ApplicationController
+  before_action :get_dates
+
   def index
+    cassandra = Cassandra.new(current_user, permitted_params)
+    @forecast = cassandra.predict
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+  end
+
+  private
+  def permitted_params
+    params.permit(
+      :start_date,
+      :end_date
+    )
+  end
+
+  def get_dates
+    if params[:start_date].blank? || params[:end_date].blank?
+      params[:start_date] = Date.today.beginning_of_month.strftime('%d %B, %Y')
+      params[:end_date] = Date.today.end_of_month.strftime('%d %B, %Y')
+    end
   end
 end
---
+++ app/services/cassandra.rb
@@ -0,0 +1,62 @@
+class Cassandra
+  attr_reader :user, :start_date, :end_date
+
+  def initialize(user, args)
+    @user = user
+    @my_money = user.initial_amount
+    @start_date = Date.parse(args[:start_date])
+    @end_date = Date.parse(args[:end_date])
+  end
+
+  def predict
+    date_range.inject({}) do |ini, date|
+      start_of_month = date.beginning_of_month
+      end_of_month = date.end_of_month
+      quarter = (date.beginning_of_year + 3.months).end_of_month
+      half_of_year = (date.beginning_of_year + 5.months).end_of_month
+
+      remove_monies(single_occurrence: date)
+
+      case
+      when date == start_of_month + 14.days
+        remove_monies(repeat: 'semi-monthly')
+      when date == end_of_month
+        # Because the end of month is also the second half of the semi-monthly
+        remove_monies(repeat: 'semi-monthly')
+
+        remove_monies(repeat: 'monthly')
+      when date == quarter
+        remove_monies(repeat: 'quarterly')
+      when date == half_of_year
+        remove_monies(repeat: 'semi-annual')
+      when date == date.end_of_year
+        # Because the end of year is also the second half of the semi-annual
+        remove_monies(repeat: 'semi-annual')
+
+        remove_monies(repeat: 'annual')
+      end
+
+      key = date.strftime('%d %B, %Y')
+      ini[key] = @my_money
+
+      ini
+    end
+  end
+
+  private
+  def date_range
+    (start_date..end_date).to_a
+  end
+
+  def remove_monies(query)
+    transactions = user.transactions.where(query)
+
+    transactions.each do |t|
+      if t.flow == 'credit'
+        @my_money += t.amount
+      else
+        @my_money -= t.amount
+      end
+    end
+  end
+end
