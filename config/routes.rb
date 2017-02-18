9dcea446b931fdb3d533bf5de107f1630e3f9959
Alfonso Juan Dillera <alfonsod@sourcepad.com>
2017-02-18 22:21:45 +0800

    Added link to forecasts.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'transactions#index'
   resources :transactions, except: [:show]
   resources :categories, only: [:index, :new, :create, :destroy]
+  resources :forecasts, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
