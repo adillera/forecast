0ee3cd593dd3482b4d1f3450f67ffc8ca13e1c82
Alfonso Juan Dillera <alfonsod@sourcepad.com>
2017-02-18 14:56:25 +0800

    Added initial styling for main menu.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'transactions#index'
   resources :transactions
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
