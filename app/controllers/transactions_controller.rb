c3b585155f29202c1819b49e51ea942b8b947f1c
Alfonso Juan Dillera <alfonsod@sourcepad.com>
2017-02-18 18:52:30 +0800

    Added saving of transaction.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -11,6 +11,13 @@ class TransactionsController < ApplicationController
   end
 
   def create
+    @transaction = current_user.transactions.new(permitted_params)
+
+    if @transaction.save
+      redirect_to transactions_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,5 +27,19 @@ class TransactionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def permitted_params
+    params[:transaction] = params[:transaction].reject{ |k, v| v.blank? }
+
+    params.require(:transaction).permit(
+      :flow,
+      :item,
+      :amount,
+      :repeat,
+      :single_occurrence,
+      :category_id
+    )
   end
 end
